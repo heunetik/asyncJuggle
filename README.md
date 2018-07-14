d6051f95a39cefbf8aa9088daff896188e45346b
heunetik <heunetik@gmail.com>
2018-07-14 13:06:44 +0300

    Added files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # asyncJuggle
 Node.js Async juggling w/o "async" package
+
+## Notes
+It only works with 3 args (all HTTPS)
---
+++ juggle.js
@@ -0,0 +1,27 @@
+const https = require('https');
+const BufferList = require('bl');
+const args = process.argv.slice(2);
+
+let resultArray = [null, null, null];
+
+https.get(args[0], callbackFactory(0));
+https.get(args[1], callbackFactory(1));
+https.get(args[2], callbackFactory(2));
+
+function callbackFactory(pos) {
+    return function cb(res) {
+        console.log(pos);
+        res.pipe(BufferList((err, data) => {
+            if(err) {
+                console.error("error");
+            }
+            resultArray[pos] = data.toString();
+            const complete = resultArray.reduce((acc, currentVal) => {
+                return acc && !!currentVal;
+            }, true);
+            if(complete) {
+                console.log(resultArray);
+            }
+        }));
+    }
+}
